e691a9616d38e0961db4a7d265411417276f40c6
Anurag Tiwari <Anurag.TA.Tiwari@bti.bmwgroup.com>
2026-09-06 16:37:35 +0530

    chore: ensure default repo is Anurag-TA-Tiwari/contextual-prompt-generator

--- scripts/install-remote.sh
+++ scripts/install-remote.sh
@@ -8,7 +8,7 @@
 set -euo pipefail
 
 DEFAULT_HOST="${DEFAULT_HOST:-bmw.ghe.com}"
-DEFAULT_REPO="${DEFAULT_REPO:-CDBase/bti-skill}"
+DEFAULT_REPO="${DEFAULT_REPO:-Anurag-TA-Tiwari/contextual-prompt-generator}"
 
 HOST="${HOST:-$DEFAULT_HOST}"
 REPO="${REPO:-$DEFAULT_REPO}"
